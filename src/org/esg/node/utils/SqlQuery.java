58a06553fc0dac5f4eec6d1751517376321d5295
Paola Nassisi <paola.nassisi@gmail.com>
2016-11-09 11:29:40 +0100

    Queries to database simplified.

--- src/org/esg/node/utils/SqlQuery.java
+++ src/org/esg/node/utils/SqlQuery.java
@@ -9,37 +9,16 @@ public enum SqlQuery {
     CLIENT_STATS_DM("SELECT * FROM esgf_dashboard.client_stats_dm"),
     
     /** cross project **/
-    CROSS_DMART_PROJECT_HOST_TIME("SELECT " +
-    		"total_size, number_of_downloads, number_of_successful_downloads, number_of_replica_downloads," +
-    		"average_duration, number_of_users, host_name, project_name, month, year " +
-    		"FROM esgf_dashboard.cross_dmart_project_host_time;"),
-    CROSS_DMART_PROJECT_HOST_GEOLOCATION("SELECT " +
-    		"total_size, number_of_downloads, number_of_successful_downloads, number_of_replica_downloads," +
-    		"average_duration, number_of_users, host_name, project_name, latitude, longitude " +
-    		"FROM esgf_dashboard.cross_dmart_project_host_geolocation;"),
+    CROSS_DMART_PROJECT_HOST_TIME("SELECT * FROM esgf_dashboard.cross_dmart_project_host_time;"),
+    CROSS_DMART_PROJECT_HOST_GEOLOCATION("SELECT * FROM esgf_dashboard.cross_dmart_project_host_geolocation;"),
     /** cross project **/
     
     /** obs4mips **/
-    OBS_DMART_CLIENTS_HOST_TIME_GEOLOCATION("SELECT " +
-    		"total_size, number_of_downloads, number_of_successful_downloads, " +
-    		"average_duration, number_of_users, month, year, latitude, longitude, host_name " +
-    		"FROM esgf_dashboard.obs4mips_dmart_clients_host_time_geolocation;"),
-    OBS_DMART_VARIABLE_HOST_TIME("SELECT " +
-    		"total_size, number_of_downloads, number_of_successful_downloads, average_duration, " +
-    		"number_of_users, month, year, host_name, variable_code, variable_long_name, cf_standard_name " +
-    		"FROM esgf_dashboard.obs4mips_dmart_variable_host_time;"),
-    OBS_DMART_SOURCE_HOST_TIME("SELECT " +
-    		"total_size, number_of_downloads, number_of_successful_downloads, average_duration, " +
-    		"number_of_users, month, year, host_name, source_id_name " +
-    		"FROM esgf_dashboard.obs4mips_dmart_source_host_time;"),
-    OBS_DMART_REALM_HOST_TIME("SELECT " +
-    		"total_size, number_of_downloads, number_of_successful_downloads, average_duration, " +
-    		"number_of_users, month, year, host_name, realm_name " +
-    		"FROM esgf_dashboard.obs4mips_dmart_realm_host_time;"),
-    OBS_DMART_DATASET_HOST_TIME("SELECT " +
-    		"total_size, number_of_downloads, number_of_successful_downloads, average_duration, " +
-    		"number_of_users, month, year, host_name, dataset_name, dataset_version, datetime_start, datetime_stop " +
-    		"FROM esgf_dashboard.obs4mips_dmart_dataset_host_time;"),
+    OBS_DMART_CLIENTS_HOST_TIME_GEOLOCATION("SELECT * FROM esgf_dashboard.obs4mips_dmart_clients_host_time_geolocation;"),
+    OBS_DMART_VARIABLE_HOST_TIME("SELECT * FROM esgf_dashboard.obs4mips_dmart_variable_host_time;"),
+    OBS_DMART_SOURCE_HOST_TIME("SELECT * FROM esgf_dashboard.obs4mips_dmart_source_host_time;"),
+    OBS_DMART_REALM_HOST_TIME("SELECT * FROM esgf_dashboard.obs4mips_dmart_realm_host_time;"),
+    OBS_DMART_DATASET_HOST_TIME("SELECT * FROM esgf_dashboard.obs4mips_dmart_dataset_host_time;"),
 	/** obs4mips **/
 
     /** cmip5 **/
